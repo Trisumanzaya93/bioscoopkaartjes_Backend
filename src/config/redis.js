197f0595c6495d8c2be7871790d0d19589b1049e
erikasempana <erika.sempana@zicare.id>
2025-04-23 16:10:01 +0700

    update redis conection

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,15 +6,22 @@ const client = createClient({
   // port: process.env.REDIS_PORT,
   // password: process.env.REDIS_PASSWORD,
   // url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
 });
 
+/*
 (async () => {
-  client.connect();
+  await client.connect();
   client.on("connect", () => {
     // eslint-disable-next-line no-console
     console.log("You're now connected db redis ...");
   });
+})();
+*/
+
+(async () => {
+  await client.connect(); // kasih await biar tunggu selesai connect
+  console.log("You're now connected to Redis!");
 })();
 
 module.exports = client;
